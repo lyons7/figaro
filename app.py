6ca27a22155db59156a729b2b694490d25b38cd8
Kate Lyons <kalyons3@illinois.edu>
2018-09-23 20:09:36 -0400

    add dynamic content

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
-def hello():
-    return "Figaro web app created by Kate Lyons."
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    trends = ''
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            word = request.form['word']
+            # print statements just print to terminal
+            print("word was:")
+            print(word)
+        except:
+            print("error")
+    return render_template('index.html')
 
+# Want to keep this always probably?
 if __name__ == '__main__':
     app.run()
