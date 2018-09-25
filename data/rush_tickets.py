daa8fdd6e44ebd8b6e83ec1b1ed65ac4597dfecf
Kate Lyons <kalyons3@illinois.edu>
2018-09-25 01:11:22 -0400

    update data

--- data/rush_tickets.py
+++ data/rush_tickets.py
@@ -14,6 +14,7 @@ import os
 import dateutil.parser
 
 
+
 # Figure out how to get stuff that we want
 os.getcwd()
 os.chdir('/Users/katelyons/Downloads/rush/20150920184759/www.metopera.org/Season/Tickets/Rush-Page/')
@@ -49,45 +50,40 @@ avail
 HtmlFile.close
 
 # Iterate!
+
 page_date = []
 perf_date = []
 avail = []
+# We know this works I think, just have to get the iteration part down...
+HtmlFile = open('index.html', 'r', encoding='utf-8')
+source_code = HtmlFile.read()
+soup = BeautifulSoup(source_code, 'html.parser')
+ # Have to do this first to make sure something is there to continue on
+try:
+# Date of performance
+    perf_date1 = soup.find('p', class_ = 'performance-list-day-text')
+    perf_date2 = re.sub('<[^>]+>', '', str(perf_date1))
+    perf_date3 = re.sub('\\n', '', str(perf_date2))
+    perf_date4 = str.strip(perf_date3)
+    perf_date5 = datetime.strptime(perf_date4, "%a, %b %d").strftime("%m-%d")
+    perf_date.append(strperf_date5)
+except:
+    perf_date1 = None
 
+    # Date of scraping
+    scripts = soup.find_all('script')
+    page_date1 = re.sub('\D', '', str(scripts[3]))
+    page_date2 = page_date1[:-8]
+    page_date3 = datetime.strptime(page_date2, "%Y%m%d").date()
+    page_date.append(str(page_date3))
 
-for root, dirs, files in os.walk("/Users/katelyons/Downloads/rush"):
-    for name in files:
-        if name.endswith((".html", ".htm")):
-            HtmlFile = open('index.html', 'r', encoding='utf-8')
-            source_code = HtmlFile.read()
-
-            soup = BeautifulSoup(source_code, 'html.parser')
-
-            # Figure out how to isolate aspects of page
-            # Date of scraping
-            scripts = soup.find_all('script')
-            page_date1 = re.sub('\D', '', str(scripts[3]))
-            page_date2 = page_date1[:-8]
-            page_date3 = datetime.strptime(page_date2, "%Y%m%d").date()
-            page_date.append(str(page_date3))
-
-            try: 
-                # Date of performance
-                perf_date1 = soup.find('p', class_ = 'performance-list-day-text')
-                perf_date2 = re.sub('<[^>]+>', '', str(perf_date1))
-                perf_date3 = re.sub('\\n', '', str(perf_date2))
-                perf_date4 = str.strip(perf_date3)
-                perf_date5 = datetime.strptime(perf_date4, "%a, %b %d").strftime("%m-%d")
-                perf_date.append(strperf_date5)
-            except:
-                perf_date1 = None
-
-            # Availability
-            avail1 = soup.find('div', class_ = 'performance-list-performance-purchase force-show-btn')
-            avail2 = re.sub('<[^>]+>', '', str(avail1))
-            avail3 = re.sub('\\n', '', str(avail2))
-            avail.append(avail3)
-            HtmlFile.close
+    # Availability
+    avail1 = soup.find('div', class_ = 'performance-list-performance-purchase force-show-btn')
+    avail2 = re.sub('<[^>]+>', '', str(avail1))
+    avail3 = re.sub('\\n', '', str(avail2))
+    avail.append(avail3)
 
 rush_data = pd.DataFrame({'page_date': page_date,
                            'perf_date': perf_date,
                            'avail': avail})
+rush_data
