c4dff28fd064596f02fa705626907103032eb50b
Kate Lyons <kalyons3@illinois.edu>
2018-09-28 12:49:10 -0400

    app dev

--- app.py
+++ app.py
@@ -28,12 +28,13 @@ def recommendations():
 
     # Capitalize somethings
     predicts['ending'] = predicts.ending.str.title()
+    predicts
 
     # Put input from user into some kind of format
     ending = str(request.form['ending'])
     kid = str(request.form['kid'])
     # length = int(request.form['length'])
-    composer = str(request.form['ending'])
+    composer = str(request.form['composer'])
     language = str(request.form['language'])
     story = str(request.form['story'])
     month = str(request.form['month'])
@@ -51,8 +52,8 @@ def recommendations():
     new_df = pd.DataFrame({'ending':[ending],'kid':[kid], 'composer':[composer], 'language':[language], 'story':[story], 'month':[month]})
 
     # Start figuring out recommendation process
-    new_list = new_df.values.tolist()
-    new_list=new_list[0]
+    new_list1 = new_df.values.tolist()
+    new_list=new_list1[0]
 
     # special = predicts[['ending','kid','length','composer','language','story','month']]
     special = predicts[['ending','kid','composer','language','story','month']]
@@ -69,34 +70,36 @@ def recommendations():
     for i in predict_list:
         sim_score1 = jaccard_similarity_score(i, new_list, normalize=False)
         sim_score.append(str(sim_score1))
-
     # Put these back with original df
     se = pd.Series(sim_score)
     predicts['sim_score'] = se.values
     reco = predicts.sort_values(['sim_score', 'popularity_predictions'])
-    cos_sims = reco.tail(1)
+    cos_sims1 = reco.tail(1)
+    cos_sims = cos_sims1.values.tolist()
+    # cos_sims
+    # for x in cos_sims:
+    #     print (x)
+        # Now we have to input all of this additional information ...
 
-    # Now we have to input all of this additional information ...
-
-    #for my app the recommender gives an image of the florist and a link
-    # florist_info = {
-    # 'Flora_by_Nora':
-    #     {'name':'Flora by Nora', 'img_src':'/static/img/flora_by_nora.png', 'link':'https://www.florabynora.com/'},
-    # 'Madelyn_Claire_Floral_Design_&_Events':
-    #     {'name':'Madlyn Claire Floral Design', 'img_src':'/static/img/madelyn_claire.png', 'link':'https://madelynclairefloraldesign.com/'},
-    # 'Little_Shop_of_Floral':
-    #     {'name':'Little Shop of Floral', 'img_src':'/static/img/little_shop_of_floral.png', 'link':'https://www.littleshopoffloral.com/'},
-    # 'Lumme_Creations':
-    #     {'name':'Lumme Creations', 'img_src':'/static/img/lumme.png', 'link':'https://www.lummecreations.com/'},
-    # 'Rooted':
-    #     {'name':'Rooted Floral and Design', 'img_src':'/static/img/rooted.png', 'link':'https://www.rootedfloralanddesign.com/'},
-    # 'Blush_&_Bay':
-    #     {'name':'Blush and Bay', 'img_src':'/static/img/blush_and_bay.png', 'link':'http://www.blushandbay.com/'}}
+        #for my app the recommender gives an image of the florist and a link
+        # florist_info = {
+        # 'Flora_by_Nora':
+        #     {'name':'Flora by Nora', 'img_src':'/static/img/flora_by_nora.png', 'link':'https://www.florabynora.com/'},
+        # 'Madelyn_Claire_Floral_Design_&_Events':
+        #     {'name':'Madlyn Claire Floral Design', 'img_src':'/static/img/madelyn_claire.png', 'link':'https://madelynclairefloraldesign.com/'},
+        # 'Little_Shop_of_Floral':
+        #     {'name':'Little Shop of Floral', 'img_src':'/static/img/little_shop_of_floral.png', 'link':'https://www.littleshopoffloral.com/'},
+        # 'Lumme_Creations':
+        #     {'name':'Lumme Creations', 'img_src':'/static/img/lumme.png', 'link':'https://www.lummecreations.com/'},
+        # 'Rooted':
+        #     {'name':'Rooted Floral and Design', 'img_src':'/static/img/rooted.png', 'link':'https://www.rootedfloralanddesign.com/'},
+        # 'Blush_&_Bay':
+        #     {'name':'Blush and Bay', 'img_src':'/static/img/blush_and_bay.png', 'link':'http://www.blushandbay.com/'}}
 
 
-    #arguments are whatever comes out of your app, in my case a cos_sim and the recommended florist
-    #the structure is render_template('your_html_file.html', x=x, y=y, etc...)
-    #refer to my recommendations.html to see how variables work
+        #arguments are whatever comes out of your app, in my case a cos_sim and the recommended florist
+        #the structure is render_template('your_html_file.html', x=x, y=y, etc...)
+        #refer to my recommendations.html to see how variables work
     return render_template('recommendations.html', cos_sims = cos_sims)
 
     # return render_template('recommendations.html', cos_sims = cos_sims, florist_info = florist_info)
